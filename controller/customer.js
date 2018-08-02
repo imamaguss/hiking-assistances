a0fca29af221237323f472e9d52f6bbc0ced5a70
andryjns <andry_jns@yahoo.com>
2018-08-02 16:42:48 +0700

    add crude customer. still not sure

---
+++ controller/customer.js
@@ -0,0 +1,88 @@
+const models = require('../models');
+const Provider = models.Provider;
+const Customer = models.Customer;
+const Booking = models.Booking;
+
+class ControllerCustomer{
+
+    static index(req, res){
+        Customer.findAll({
+            order : [['id','ASC']],
+            //include: [models.Provider] 
+        })
+        .then(customerData => {
+            res.render('customers/customers.ejs', {customerData:customerData})
+        })
+    
+        .catch(err => {
+            res.send(err)
+        })
+    }  
+
+    static addGet(req, res){
+        res.render('customers/addCustomer.ejs')
+    }
+
+    static addPost(req, res){
+        Customer.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            createAt: new Date(),
+            updateAt: new Date()
+        })
+        .then(newCustomer => {
+            res.redirect('/customer')
+        })
+        .catch(err => {
+            res.send(err)
+        })
+    }
+
+    static editGet(req,res) {
+        let id = req.params.id
+        Customer.findById(id)
+        .then(editCustomer =>{
+            res.render("customers/editCustomer.ejs",{editCustomer:editCustomer})
+        })
+        .catch(err => {
+            res.send(err)
+        })
+    }
+
+    static editPost(req,res) {
+        Customer.update({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        },{
+         where:{
+         id:req.params.id
+        }
+        })
+        .then(() =>{
+            res.redirect("/customers")
+        })
+        .catch(err => {
+            res.send(err)
+        })
+    }
+
+    static delete(req,res) {
+        let id = req.params.id
+        Customer.destroy({
+            where:{id : id}
+        })
+        .then(()=>{
+            res.redirect('/customer')
+        })
+        .catch(err => {
+            res.send(err)
+        })
+    } 
+
+
+    
+}
+
+module.exports = ControllerCustomer
---
+++ router/customer.js
@@ -0,0 +1,21 @@
+'use strict'
+const express = require('express')
+const router = express.Router()
+const models = require('../models')
+const Customer= models.Customer
+const ControllerCustomer= require ('../controller/customer')
+
+router.get('/customer', ControllerCustomer.index)
+
+router.get('/customer/add', ControllerCustomer.addGet )
+
+router.post('/customer/add', ControllerCustomer.postGet)	
+
+router.get('/customer/edit/:id', ControllerCustomer.editGet)
+
+router.post('/customer/edit/:id',ControllerCustomer.editPost)
+
+router.get('/customer/delete/:id',ControllerCustomer.delete)
+
+
+module.exports = router
